0105e9d8820f71c4f24efaff633ef82bae724c97
ToshqulovLaziz <toshqulovlaziz04@gmail.com>
2022-12-31 08:43:42 +0500

    pokemon done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const elHamburgerBtn = document.querySelector (".item__bottom") ;
-const elHeader = document.querySelector (".item") ;
+const elHeader = document.querySelector (".pokemon-card") ;
 
 elHamburgerBtn.addEventListener ('click', ()=> {
     elHeader.classList.toggle("header__open") ;
